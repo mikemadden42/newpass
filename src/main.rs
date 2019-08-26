15d9e48014e55805e8039529cf91429cc8742685
Michael Madden <mikemadden42@icloud.com>
2019-08-26 13:38:07 -0500

    Initial import

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+extern crate rand;
+
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+
+fn main() {
+    let rand_string: String = thread_rng().sample_iter(&Alphanumeric).take(30).collect();
+
+    println!("{}", rand_string);
+}
