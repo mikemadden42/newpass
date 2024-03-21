b86cb1a66f392dfb21dceeedb2cfff73268151a1
Michael Madden <mikemadden42@icloud.com>
2024-03-21 06:31:26 -0500

    Improve error handling.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
-use std::env;
+use std::{env, process};
 
 fn main() {
     for argument in env::args().skip(1) {
+        // Attempt to parse the argument into an integer
+        let length: usize = match argument.parse() {
+            Ok(num) => num,
+            Err(_) => {
+                eprintln!("Error: Couldn't parse '{argument}' as a valid number");
+                process::exit(1);
+            }
+        };
+
+        // Generate random string of given length
         let rand_string: String = thread_rng()
             .sample_iter(&Alphanumeric)
-            .take(argument.parse().unwrap())
+            .take(length)
             .map(char::from)
             .collect();
-        println!("{}", rand_string);
+
+        println!("{rand_string}");
     }
 }
